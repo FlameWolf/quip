c2d9351825c69bea7e925360d5dc7f2b6ce397fa
FlameWolf <ajifocus@LIVE.COM>
2022-02-03 11:52:24 +0530

    Add find by id method to store

--- src/store/quip-store.jsx
+++ src/store/quip-store.jsx
@@ -8,5 +8,21 @@ export const [quipStore, setQuipStore] = createStore({
 		{ id: 4, content: "I have no idea.", replyTo: 3, hasReplies: false },
 		{ id: 5, content: "This is so werid!", replyTo: null, hasReplies: false }
 	],
-	nextId: 6
+	nextId: 6,
+	findQuipById: id => {
+		const findRecursive = (quips, id) => {
+			let foundQuip = null;
+			for (const quip of quips) {
+				const replies = quip.replies;
+				if (quip.id === id) {
+					foundQuip = quip;
+					break;
+				} else if (!foundQuip && replies) {
+					foundQuip = findRecursive(replies, id);
+				}
+			}
+			return foundQuip;
+		};
+		return findRecursive(quipStore.quips, id);
+	}
 });
