6a38af2f3230da3a7f556b5a57f22f980c5e97a9
FlameWolf <ajifocus@LIVE.COM>
2022-02-10 14:34:20 +0530

    Use Inter typeface as an NPM package instead of importing from Google Fonts

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
-import { render } from "solid-js/web";
+import "inter-ui/inter.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
-import App from "./App";
+import { render } from "solid-js/web";
 import { Router } from "solid-app-router";
+import App from "./App";
 
 render(
 	() => (
