ab8fb31ae1e73136df50429ba3daf7dbb3b52b43
FlameWolf <ajifocus@LIVE.COM>
2022-03-27 15:37:55 +0530

    Add sign up API call

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,10 +1,12 @@
+import { Popover } from "bootstrap";
+import { useNavigate } from "solid-app-router";
+import { AiOutlineInfoCircle } from "solid-icons/ai";
 import { BsEye, BsEyeSlash } from "solid-icons/bs";
 import { createSignal, onMount, Show } from "solid-js";
-import { handleRegExp, passwordRegExp, invalidHandles } from "../library";
-import { AiOutlineInfoCircle } from "solid-icons/ai";
-import { Popover } from "bootstrap";
-import { userStore, setUserStore} from "../stores/user-store";
-import { useNavigate } from "solid-app-router";
+import { handleRegExp, passwordRegExp } from "../library";
+import { secureFetch } from "../secure-fetch";
+import { setAuthStore } from "../stores/auth-store";
+import { setUserStore, userStore } from "../stores/user-store";
 
 export default props => {
 	let signUpForm;
@@ -47,50 +49,53 @@ export default props => {
 		confirmPasswordInput.classList.remove("is-valid");
 		confirmPasswordInput.classList.remove("is-invalid");
 	};
-	const submitForm = (event) => {
+	const submitForm = async event => {
+		const signInUrl = `${import.meta.env.VITE_API_BASE_URL}/auth/sign-up`;
 		const username = usernameInput.value;
 		const password = passwordInput.value;
-		setUsernameUnavailable(
-			userStore.users.find(user => user.handle === username) ||
-			invalidHandles.indexOf(username) > -1
-		);
-		if(!usernameUnavailable()) {
+		const response = await secureFetch(signInUrl, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({ handle: username, password })
+		});
+		if (response.status === 201) {
+			const data = await response.json();
+			delete data.userId;
+			setAuthStore(data);
+		} else if (response.status >= 400) {
+			const body = await response.text();
+		}
+		setUsernameUnavailable(false);
+		if (!usernameUnavailable()) {
 			const newUser = {
 				id: userStore.nextId,
 				handle: username,
 				password
 			};
-			setUserStore("users", users => [...users, newUser ]);
+			setUserStore("users", users => [...users, newUser]);
 			setUserStore("nextId", value => value + 1);
 			setUserStore("currentUser", value => newUser);
-			navigate("/home", { resolve: false });
+			// navigate("/home", { resolve: false });
 		}
 	};
 	onMount(() => {
-		new Popover(
-			usernameInfoToggle,
-			{
-				content: "Username must start with a letter, be 4-32 characters long, and may contain underscores and digits",
-				trigger: "focus",
-				placement: "auto"
-			}
-		);
-		new Popover(
-			passwordInfoToggle,
-			{
-				content: "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one digit",
-				trigger: "focus",
-				placement: "auto"
-			}
-		);
-		new Popover(
-			confirmPasswordInfoToggle,
-			{
-				content: "Password entered here should match the password entered in the previous input box",
-				trigger: "focus",
-				placement: "auto"
-			}
-		);
+		new Popover(usernameInfoToggle, {
+			content: "Username must start with a letter, be 4-32 characters long, and may contain underscores and digits",
+			trigger: "focus",
+			placement: "auto"
+		});
+		new Popover(passwordInfoToggle, {
+			content: "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one digit",
+			trigger: "focus",
+			placement: "auto"
+		});
+		new Popover(confirmPasswordInfoToggle, {
+			content: "Password entered here should match the password entered in the previous input box",
+			trigger: "focus",
+			placement: "auto"
+		});
 	});
 	return (
 		<form ref={signUpForm} onInput={updateFormValidity}>
@@ -103,36 +108,44 @@ export default props => {
 			<div class="d-flex mb-2">
 				<label>Username</label>
 				<a ref={usernameInfoToggle} class="ms-auto clickable" tabIndex={-1}>
-					<AiOutlineInfoCircle/>
+					<AiOutlineInfoCircle />
 				</a>
 			</div>
 			<div class="input-group mb-4">
 				<span class="input-group-text">@</span>
-				<input ref={usernameInput} class="form-control" type="text" placeholder="Username" pattern={handleRegExp.source}/>
+				<input ref={usernameInput} class="form-control" type="text" placeholder="Username" pattern={handleRegExp.source} />
 			</div>
 			<div class="d-flex mb-2">
 				<label>Password</label>
 				<a ref={passwordInfoToggle} class="ms-auto clickable" tabIndex={-1}>
-					<AiOutlineInfoCircle/>
+					<AiOutlineInfoCircle />
 				</a>
 			</div>
 			<div class="input-group mb-4">
-				<input ref={passwordInput} class="form-control" type={showPassword() ? "text" : "password"} placeholder="Password" pattern={passwordRegExp.source}/>
-				<span class="input-group-text clickable" onClick={event => setShowPassword(value => !value)}>{showPassword() ? BsEyeSlash : BsEye}</span>
+				<input ref={passwordInput} class="form-control" type={showPassword() ? "text" : "password"} placeholder="Password" pattern={passwordRegExp.source} />
+				<span class="input-group-text clickable" onClick={event => setShowPassword(value => !value)}>
+					{showPassword() ? BsEyeSlash : BsEye}
+				</span>
 			</div>
 			<div class="d-flex mb-2">
 				<label>Confirm password</label>
 				<a ref={confirmPasswordInfoToggle} class="ms-auto clickable" tabIndex={-1}>
-					<AiOutlineInfoCircle/>
+					<AiOutlineInfoCircle />
 				</a>
 			</div>
 			<div class="input-group mb-4">
-				<input ref={confirmPasswordInput} class="form-control" type={showConfirmPassword() ? "text" : "password"} placeholder="Confirm password"/>
-				<span class="input-group-text clickable" onClick={event => setShowConfirmPassword(value => !value)}>{showConfirmPassword() ? BsEyeSlash : BsEye}</span>
+				<input ref={confirmPasswordInput} class="form-control" type={showConfirmPassword() ? "text" : "password"} placeholder="Confirm password" />
+				<span class="input-group-text clickable" onClick={event => setShowConfirmPassword(value => !value)}>
+					{showConfirmPassword() ? BsEyeSlash : BsEye}
+				</span>
 			</div>
 			<div class="d-flex">
-				<button class="btn btn-secondary w-50 me-1" type="button" disabled={!formHasValue()} onClick={resetForm}>Clear Form</button>
-				<button class="btn btn-primary w-50 ms-1" type="button" disabled={!formValidity()} onClick={submitForm}>Sign Up</button>
+				<button class="btn btn-secondary w-50 me-1" type="button" disabled={!formHasValue()} onClick={resetForm}>
+					Clear Form
+				</button>
+				<button class="btn btn-primary w-50 ms-1" type="button" disabled={!formValidity()} onClick={submitForm}>
+					Sign Up
+				</button>
 			</div>
 		</form>
 	);
