9232bea8c93885d976b5379d0d317eff8bb65d08
FlameWolf <ajifocus@LIVE.COM>
2022-03-28 16:11:38 +0530

    Add `userId` and `handle` fields

--- src/stores/auth-store.jsx
+++ src/stores/auth-store.jsx
@@ -1,6 +1,8 @@
 import { createStore } from "solid-js/store";
 
 export const [authStore, setAuthStore] = createStore({
+	userId: undefined,
+	handle: undefined,
 	token: undefined,
 	createdAt: undefined,
 	expiresIn: undefined
