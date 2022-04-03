b19ba087e27ad91d1a494c8fb46e8e9e6babef06
FlameWolf <ajifocus@LIVE.COM>
2022-04-03 10:21:54 +0530

    Implement API call for loading home timeline

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,35 @@
-import Editor from "./Editor";
-import DisplayPost from "./DisplayPost";
-import { quipStore } from "../stores/quip-store";
-import { createMemo, For } from "solid-js";
+import { createMemo, createSignal, For, onMount } from "solid-js";
 import { trimPost } from "../library";
+import { quipStore, setQuipStore } from "../stores/quip-store";
+import DisplayPost from "./DisplayPost";
+import Editor from "./Editor";
 
 export default props => {
+	let loadMoreButton;
+	const postsUrl = `${import.meta.env.VITE_API_BASE_URL}/timeline`;
+	const [lastPostId, setLastPostId] = createSignal("");
+	const loadPosts = async (lastPostId = undefined) => {
+		const response = await fetch(lastPostId ? `${postsUrl}?lastPostId=${lastPostId}` : postsUrl);
+		if (response.status === 200) {
+			const posts = (await response.json()).posts;
+			if (posts.length) {
+				setQuipStore("quips", quips => [...quips, ...posts]);
+				setLastPostId(posts.at(-1)._id);
+				return;
+			}
+			loadMoreButton.textContent = "No More Posts";
+			loadMoreButton.disabled = true;
+		}
+	};
+	onMount(async () => {
+		await loadPosts();
+	});
 	const renderRecursive = (post, isReply = false, parentBlurb = undefined) => {
-		const replies = createMemo(() => quipStore.quips.filter(reply => reply.replyTo === post.id));
+		const replies = createMemo(() => quipStore.quips.filter(reply => reply.replyTo === post._id));
 		return (
 			<>
 				<DisplayPost post={post} hasReplies={replies().length} isReply={isReply} parentBlurb={parentBlurb}/>
-				<For each={replies()}>
-				{
-					(reply, index) => renderRecursive(reply, true, trimPost(post.content))
-				}
-				</For>
+				<For each={replies()}>{(reply, index) => renderRecursive(reply, true, trimPost(post.content))}</For>
 			</>
 		);
 	};
@@ -22,11 +37,10 @@ export default props => {
 		<>
 			<Editor id="post-editor" classList={{ "mb-2": true }}/>
 			<ul class="list-group">
-				<For each={quipStore.quips}>
-				{
-					(quip, index) => !quip.replyTo && renderRecursive(quip)
-				}
-				</For>
+				<For each={quipStore.quips}>{(quip, index) => !quip.replyTo && renderRecursive(quip)}</For>
+				<li class="list-group-item">
+					<button ref={loadMoreButton} class="btn btn-primary form-control" onClick={() => loadPosts(lastPostId())}>Load More</button>
+				</li>
 			</ul>
 		</>
 	);
