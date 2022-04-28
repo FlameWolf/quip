a83fb02497dd376c720200933e2e2f13cf3acb86
FlameWolf <agnijith@gmail.com>
2022-04-28 19:16:50 +0530

    Create DisplayPostMinimal.jsx

---
+++ src/components/DisplayPostMinimal.jsx
@@ -0,0 +1,23 @@
+import { Show } from "solid-js";
+
+export default props => {
+	const post = props.post;
+	const handle = post.author.handle;
+	return (
+		<>
+			<div data-post-id={post._id} class="list-group-item p-0 rounded" classList={{ "reply": props.isReply }}>
+				<div class="post-header">
+					<div>@</div>
+					<a class="author.handle" href={`/${handle}`}>{handle}</a>
+					<Show when={props.parentBlurb}>
+						<div>&#xA0;</div>
+						<div innerHTML={`Replied to "${props.parentBlurb}"`}></div>
+					</Show>
+				</div>
+				<div class="card-body">
+					<p class="card-text" innerHTML={post.content?.replace(/\n/g, "<br/>")}></p>
+				</div>
+			</div>
+		</>
+	);
+};
