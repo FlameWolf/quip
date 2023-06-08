aef025cc20fca88010f4c1331a99c83c3873d97d
FlameWolf <agnijith@gmail.com>
2023-06-08 17:03:55 +0530

    Implement profile loading from API

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,34 +1,32 @@
 import { useParams } from "@solidjs/router";
-import { createMemo, For } from "solid-js";
-import { quipStore } from "../stores/quip-store";
-import { userStore } from "../stores/user-store";
+import { createResource, For, Show, Suspense } from "solid-js";
 import DisplayPost from "./DisplayPost";
 
 export default props => {
 	const params = useParams();
-	const profileUser = userStore.users.find(
-		user => user.handle.toLowerCase() === params.handle.toLowerCase()
-	);
-	const userQuips = createMemo(
-		() => quipStore.quips.filter(quip => quip.author._id === profileUser.id)
-	);
+	const [profileUser] = createResource(async handle => await (await fetch(`${import.meta.env.VITE_API_BASE_URL}users/${params.handle}`)).json());
+	const [userQuips, { mutate, refetch }] = createResource(async handle => await (await fetch(`${import.meta.env.VITE_API_BASE_URL}users/${params.handle}/posts`)).json());
 	return (
-		<>
-			<div class="card mb-2">
-				<div class="card-header">
-					<div class="card-title">@{profileUser.handle}</div>
+		<Suspense fallback={<h4>Loading...</h4>}>
+			<Show when={profileUser.state === "ready"}>
+				<div class="card mb-2">
+					<div class="card-header">
+						<div class="card-title">@{profileUser().user.handle}</div>
+					</div>
+					<div class="card-body">
+						<p>{userQuips()?.length} Quips</p>
+					</div>
 				</div>
-				<div class="card-body">
-					<p>{userQuips().length} Quips</p>
-				</div>
-			</div>
+			</Show>
 			<ul class="list-group">
-				<For each={userQuips()}>
-				{
-					(quip, index) => <DisplayPost post={quip}/>
-				}
-				</For>
+				<Show when={userQuips.state === "ready"}>
+					<For each={userQuips().posts}>
+					{
+						(quip, index) => <DisplayPost post={quip}/>
+					}
+					</For>
+				</Show>
 			</ul>
-		</>
+		</Suspense>
 	);
 };
