199af8d3a1c352d2bba0ec37d47255553e3b560d
FlameWolf <agnijith@gmail.com>
2023-06-13 14:14:06 +0530

    Update posts count population logic; refactor code

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,18 +1,23 @@
 import { useParams } from "@solidjs/router";
-import { createResource, createSignal, For, onMount, Show, Suspense } from "solid-js";
+import { createSignal, For, onMount, Show, Suspense } from "solid-js";
 import { maxPostsToFetch } from "../library";
 import DisplayPost from "./DisplayPost";
 
 export default props => {
 	let loadMoreButton;
 	const params = useParams();
+	const [profileUser, setProfileUser] = createSignal("");
 	const [lastPostId, setLastPostId] = createSignal("");
 	const [hasMore, setHasMore] = createSignal(true);
 	const [loadedQuips, setLoadedQuips] = createSignal([]);
-	const [profileUser] = createResource(async handle => await (await fetch(`${import.meta.env.VITE_API_BASE_URL}users/${params.handle}`)).json());
+
+	const loadUser = async handle => {
+		const data = await (await fetch(`${import.meta.env.VITE_API_BASE_URL}/users/${params.handle}`)).json();
+		setProfileUser(data.user);
+	};
 
 	const loadUserQuips = async handle => {
-		const data = await (await fetch(`${import.meta.env.VITE_API_BASE_URL}users/${params.handle}/posts?lastPostId=${lastPostId()}`)).json();
+		const data = await (await fetch(`${import.meta.env.VITE_API_BASE_URL}/users/${params.handle}/posts?lastPostId=${lastPostId()}`)).json();
 		const posts = data.posts;
 		const postsCount = posts.length;
 		if(postsCount) {
@@ -22,25 +27,23 @@ export default props => {
 			setHasMore(false);
 		}
 		setLoadedQuips([...loadedQuips(), ...posts]);
-		return loadedQuips();
 	};
 
-	onMount(() => {
-		loadUserQuips();
+	onMount(async () => {
+		await loadUser();
+		await loadUserQuips();
 	});
 
 	return (
-		<Suspense fallback={<h4>Loading...</h4>}>
-			<Show when={profileUser.state === "ready"}>
-				<div class="card mb-2">
-					<div class="card-header">
-						<div class="card-title">@{profileUser().user.handle}</div>
-					</div>
-					<div class="card-body">
-						<p>{loadedQuips()?.length} Quips</p>
-					</div>
+		<>
+			<div class="card mb-2">
+				<div class="card-header">
+					<div class="card-title">@{profileUser().handle}</div>
 				</div>
-			</Show>
+				<div class="card-body">
+					<p>{profileUser().postsCount} Quips</p>
+				</div>
+			</div>
 			<ul class="list-group">
 				<For each={loadedQuips()}>
 				{
@@ -51,6 +54,6 @@ export default props => {
 					<button ref={loadMoreButton} class="btn btn-primary form-control" innerHTML={hasMore() ? "Load More" : "No More Posts"} onClick={loadUserQuips}></button>
 				</li>
 			</ul>
-		</Suspense>
+		</>
 	);
 };
