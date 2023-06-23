290aa89a6fc6e8355d82b7eada94dd55d7dcbcbd
FlameWolf <agnijith@gmail.com>
2023-06-23 12:10:01 +0530

    Replace logo with new version; modify authentication page layout

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,34 +1,32 @@
-import * as nodeEmoji from "node-emoji";
 import { NavLink, Outlet } from "@solidjs/router";
-import logoUrl from "../assets/images/logo.png";
+import logoUrl from "../assets/images/logo.svg";
 
 export default props => {
 	return (
-		<div class="pb-5 auth-panel">
-			<div class="d-flex flex-column align-items-center pb-5">
+		<div class="row justify-content-center mt-md-5">
+			<div class="col-sm-1 logo-panel my-lg-auto mx-lg-5 ms-xl-0 me-xl-5">
 				<div class="d-flex justify-content-center mb-4">
 					<img class="logo img-fluid" src={logoUrl} alt="Quip Logo"/>
 				</div>
-				<div class="hstack gap-2 justify-content-center w-100 py-2">
+				<div class="hstack gap-2 justify-content-center py-2 mb-5">
 					<span class="fw-bold fst-italic">noun</span>
 					<span>&#xB7;</span>
 					<span>a witty remark</span>
 				</div>
 			</div>
-			<div class="card">
-				<div class="card-header">
-					<div class="hstack gap-3">
-						<NavLink class="ms-auto" target="_self" href="sign-up">Sign Up</NavLink>
-						<div class="vr"></div>
-						<NavLink class="me-auto" target="_self" href="sign-in">Sign In</NavLink>
+			<div class="col-sm-11 form-panel">
+				<div class="card">
+					<div class="card-header">
+						<div class="hstack gap-3">
+							<NavLink class="ms-auto" target="_self" href="sign-up">Sign Up</NavLink>
+							<div class="vr"></div>
+							<NavLink class="me-auto" target="_self" href="sign-in">Sign In</NavLink>
+						</div>
+					</div>
+					<div class="card-body text-center py-3">
+						<Outlet/>
 					</div>
 				</div>
-				<div class="card-body text-center py-3">
-					<Outlet/>
-				</div>
-			</div>
-			<div class="d-flex flex-fill align-items-center p-2 mt-5">
-				<div class="marquee-x">{nodeEmoji.get(":slightly_smiling_face:")}</div>
 			</div>
 		</div>
 	);
