0d526662d1044d0b4ec17aae0686888c1bda8173
FlameWolf <agnijith@gmail.com>
2023-06-23 14:14:14 +0530

    Update authentication page layout

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -3,18 +3,20 @@ import logoUrl from "../assets/images/logo.svg";
 
 export default props => {
 	return (
-		<div class="row justify-content-center mt-md-5">
-			<div class="col-sm-1 logo-panel my-lg-auto mx-lg-5 ms-xl-0 me-xl-5">
-				<div class="d-flex justify-content-center mb-4">
-					<img class="logo img-fluid" src={logoUrl} alt="Quip Logo"/>
-				</div>
-				<div class="hstack gap-2 justify-content-center py-2 mb-5">
-					<span class="fw-bold fst-italic">noun</span>
-					<span>&#xB7;</span>
-					<span>a witty remark</span>
+		<div class="hstack gap-2 flex-column flex-md-row align-items-stretch auth-panel">
+			<div class="col-12 col-md-5">
+				<div class="bg-secondary-subtle h-100 p-5 rounded">
+					<div class="d-flex justify-content-center mb-4">
+						<img class="logo" src={logoUrl} alt="Quip Logo"/>
+					</div>
+					<div class="hstack gap-2 justify-content-center py-2">
+						<span class="fw-bold fst-italic">noun</span>
+						<span>&#xB7;</span>
+						<span>a witty remark</span>
+					</div>
 				</div>
 			</div>
-			<div class="col-sm-11 form-panel">
+			<div class="col-12 col-md-7">
 				<div class="card">
 					<div class="card-header">
 						<div class="hstack gap-3">
