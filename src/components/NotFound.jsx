b934aea26e0f5923f0fad07100c54f371fe32294
FlameWolf <agnijith@gmail.com>
2026-03-24 18:25:21 +0530

    Add missing shrug emojo in 404 page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,7 +3,7 @@ import * as nodeEmoji from "node-emoji";
 export default props => {
 	const pageFacingUpEmojo = nodeEmoji.get(":page_facing_up:");
 	const noEntryEmojo = nodeEmoji.get(":no_entry_sign:");
-	const shrugEmojo = nodeEmoji.get(":shrug:");
+	const shrugEmojo = nodeEmoji.get(":woman_shrugging:");
 	return (
 		<div class="position-absolute-middle text-center w-100 p-5">
 			<h1 class="text-primary">{pageFacingUpEmojo} Page {pageFacingUpEmojo}</h1>
