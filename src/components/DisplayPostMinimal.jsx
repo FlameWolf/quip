ce8d51386f48b64652ce039450fb4b29d7b848da
FlameWolf <agnijith@gmail.com>
2026-05-01 17:33:53 +0530

    Fix display of attachments in minimal post view

--- src/components/DisplayPostMinimal.jsx
+++ src/components/DisplayPostMinimal.jsx
@@ -1,10 +1,15 @@
 import { Show } from "solid-js";
 import { A } from "@solidjs/router";
-import { formatTimeAgo, parseContent, toLongDateString } from "../library";
+import { formatTimeAgo, parseContent, toLongDateString, trimPost } from "../library";
+import { FaSolidImage, FaSolidListDots } from "solid-icons/fa";
+import { RiDocumentFileTextFill } from "solid-icons/ri";
 
 export default props => {
 	const post = props.post;
 	const createdAt = post.createdAt;
+	const attachments = post.attachments;
+	const { poll, mediaFile, post: attachedPost } = attachments || {};
+	const { attachments: nestedAttachments } = attachedPost || {};
 	const handle = post.author.handle;
 	return (
 		<>
@@ -24,28 +29,62 @@ export default props => {
 						<Show when={post.content}>
 							<span innerHTML={parseContent(post.content)}/>
 						</Show>
-						<Show when={post.attachments?.mediaFile}>
-							<div class="mt-2">
-								<img src={post.attachments.mediaFile.src} class="img-fluid rounded"/>
-							</div>
-						</Show>
-						<Show when={post.attachments?.poll}>
+						<Show when={poll}>
 							<div class="mt-2">
 								<div class="card">
 									<div class="card-body py-3">
 										<div class="card-title">Poll</div>
 										<ul class="list-group list-group-flush">
-											<li class="list-group-item">{post.attachments.poll.first}</li>
-											<li class="list-group-item">{post.attachments.poll.second}</li>
-											<Show when={post.attachments.poll.third}>
-												<li class="list-group-item">{post.attachments.poll.third}</li>
+											<li class="list-group-item">{poll.first}</li>
+											<li class="list-group-item">{poll.second}</li>
+											<Show when={poll.third}>
+												<li class="list-group-item">{poll.third}</li>
 											</Show>
-											<Show when={post.attachments.poll.fourth}>
-												<li class="list-group-item">{post.attachments.poll.fourth}</li>
+											<Show when={poll.fourth}>
+												<li class="list-group-item">{poll.fourth}</li>
 											</Show>
 										</ul>
 									</div>
 								</div>
+							</div>
+						</Show>
+						<Show when={mediaFile}>
+							<div class="mt-2">
+								<Show when={mediaFile.fileType === "image"}>
+									<img class="img-fluid" alt={mediaFile.description} src={mediaFile.src}/>
+								</Show>
+								<Show when={mediaFile.fileType === "video"}>
+									<video class="img-fluid" alt={mediaFile.description} src={mediaFile.src} controls={true}/>
+								</Show>
+							</div>
+						</Show>
+						<Show when={attachedPost}>
+							<div class="border rounded mt-2">
+								<ul class="list-group list-group-flush">
+									<Show when={attachedPost.content}>
+										<li class="list-group-item">
+											<span innerHTML={trimPost(attachedPost.content)}></span>
+										</li>
+									</Show>
+									<Show when={nestedAttachments?.poll}>
+										<li class="list-group-item">
+											<span><FaSolidListDots/></span>
+											<span class="fst-italic">&#xA0;[Poll]</span>
+										</li>
+									</Show>
+									<Show when={nestedAttachments?.mediaFile}>
+										<li class="list-group-item">
+											<span><FaSolidImage/></span>
+											<span class="fst-italic">&#xA0;[Media]</span>
+										</li>
+									</Show>
+									<Show when={typeof attachedPost === "string" || nestedAttachments?.post}>
+										<li class="list-group-item">
+											<span><RiDocumentFileTextFill/></span>
+											<span class="fst-italic">&#xA0;[Quote]</span>
+										</li>
+									</Show>
+								</ul>
 							</div>
 						</Show>
 					</p>
